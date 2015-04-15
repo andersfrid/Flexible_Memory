3162ea03bad8d6b8d0a915fc3faf450f8c4964e1
appelqvist <andreas.appelqvist@gmail.com>
2015-04-15 16:51:12 +0200

    Sammanställde dagens arbete på RoundController, Ska fungera att skapa en array med bilder ur en map

--- src/GUI/RoundController.java
+++ src/GUI/RoundController.java
@@ -12,14 +12,14 @@ import java.util.Random;
  *
  */
 public class RoundController {
-	//temp
+	// temp
 	private String background = "/maps/background/background_0";
-	
+
 	private Card[][] currentGameBoard;
 	private Random rand = new Random();
-	
+
 	private int col, row;
-	
+
 	public RoundController() {
 		createNewGameBoard(0, 0);
 	}
@@ -35,39 +35,67 @@ public class RoundController {
 	 */
 	public void createNewGameBoard(int level, int mode) {
 		currentGameBoard = calcGameBoardSize(level);
-		
-		String imagePath = getModePath(mode);	
-		String folderPath = imagePath.substring(1,imagePath.lastIndexOf('/')+1);
-		
+
+		String imagePath = getModePath(mode);
+		String folderPath = imagePath.substring(0,
+				imagePath.lastIndexOf('/') + 1);
+
 		System.out.println(imagePath);
 		System.out.println(folderPath);
-		
-		int nbrOfCards = (new File(folderPath).listFiles().length)/2;
+
+		int nbrOfCards = (new File(folderPath).listFiles().length) / 2;
 		System.out.println(nbrOfCards);
-		
+
 		System.out.println(rand.nextInt(nbrOfCards));
-		
-		int[] alreadyPlaced = new int[20]; 
-		
+
+		int[] alreadyPlaced = new int[20];
+		int count = 0;
+
 		System.out.println(currentGameBoard.length);
-		
-//		while(!isFilled(currentGameBoard)){
-//			int pairNbr = rand.nextInt(nbrOfCards);
-//			if(!alreadyPlaced(alreadyPlaced, pairNbr)){
-//				
-//				if(currentGameBoard[rand.nextInt(currentGameBoard.length-1)][] != null){
-//					
-//				}
-//				
+
+		while (!isFilled(currentGameBoard)) {
+
+			int pairNbr = rand.nextInt(nbrOfCards);
+
+			//if (!alreadyPlaced(alreadyPlaced, pairNbr)) {
+				for (int i = 0; i <= 1; i++) {
+					boolean ok = false;
+
+					do {
+						int randCol = rand.nextInt(this.col);
+						int randRow = rand.nextInt(this.row);
+
+						if (currentGameBoard[randCol][randRow] == null) {
+							String pathToImage = imagePath;
+							pathToImage += "" + pairNbr + "_" + i;
+
+							alreadyPlaced[count] = pairNbr; // count läggs till
+															// efter loopen
+
+							currentGameBoard[randCol][randRow] = new Card(pathToImage, pairNbr);
+							ok = true;
+						}
+					} while (!ok);
+				}
+				count++;
 //			}
-//		}
-		
-		
+
+		}
+		System.out.println("toString");
+
+		System.out.println(currentGameBoard.toString());
+		for(int i=0; i<currentGameBoard.length; i++){
+			for(int j=0; j<currentGameBoard[i].length; j++){
+				System.out.print(currentGameBoard[i][j].getCardFront().toString()+", ");
+				if(j == currentGameBoard[i].length-1)
+					System.out.println("");
+			}
+		}
 	}
-	
-	public boolean alreadyPlaced(int[] array, int nbr){
-		for(int i = 0; i < array.length; i++){
-			if(array[i] == nbr){
+
+	public boolean alreadyPlaced(int[] array, int nbr) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == nbr) {
 				return true;
 			}
 		}
@@ -79,17 +107,17 @@ public class RoundController {
 	 * @param array
 	 * @return
 	 */
-	public boolean isFilled(Card[][] array){
-		for(int i = 0; i < array.length; i++){
-			for(int j = 0; j < array[i].length; j++){
-				if(array[i][j] == null){
+	public boolean isFilled(Card[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (array[i][j] == null) {
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Räknar ut hur spelplanen kommer se ut, skapar planen samt returnerar den
 	 * som en array[][]
@@ -120,31 +148,31 @@ public class RoundController {
 		Card[][] gameBoard = new Card[col][row];
 		return gameBoard;
 	}
-	
+
 	/**
 	 * Får filvägen till den spelläge som har valts.
+	 * 
 	 * @param mode
 	 * @return filvägen till bilderna utan till de spelläget som ska köras.
 	 */
-	public String getModePath(int mode){
+	public String getModePath(int mode) {
 		String path = "";
-		switch(mode){
-			case 0: //Standardläge
-				path = "/maps/standard/standard_";
-				break;
-			case 1: //flaggor
-				path = "/maps/flag/flag_";
-				break;
-			default :
-				path = "/maps/standard/standard_";
-				break;
-				
-			//Fler spellägen ska läggas in här!
+		switch (mode) {
+		case 0: // Standardläge
+			path = "maps/standard/standard_";
+			break;
+		case 1: // flaggor
+			path = "maps/flag/flag_";
+			break;
+		default:
+			path = "maps/standard/standard_";
+			break;
+
+		// Fler spellägen ska läggas in här!
 		}
 		return path;
 	}
 
-	
 	/**
 	 * Returnerar spelplanen
 	 * 
@@ -160,8 +188,7 @@ public class RoundController {
 	public void paintGameBoard() {
 
 	}
-	
-	
+
 	public static void main(String[] args) {
 		new RoundController();
 	}
