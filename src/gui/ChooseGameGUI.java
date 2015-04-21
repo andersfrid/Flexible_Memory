0d134619e4e1dfbac879dbb1bb29e7aa17eeff14
Joakim1331 <joakim_naw@hotmail.com>
2015-04-21 13:03:20 +0200

    la in nya gameChooser

--- src/gui/ChooseGameGUI.java
+++ src/gui/ChooseGameGUI.java
@@ -33,24 +33,23 @@ import controller.ControllerGUI;
 public class ChooseGameGUI extends JPanel implements ActionListener {
 
 	private JFrame frame = new JFrame("Flexible Memory");
-	private JPanel mainPanel, contentPanel, bg, centerPanel;
-	private JButton btnEasy = new JButton("Easy");
-	private JButton btnMedium = new JButton("Medium");
-	private JButton btnHard = new JButton("Hard");
-	private JButton btnStart = new JButton("Start Game!");
-	private JButton btnSettings;
+	private JPanel mainPanel, contentPanel, bg;
+	private JButton btnEasy, btnMedium, btnHard, btnSettings, btnStart, btnStandard, btnFlag, btnPlus;
 	private JLabel lblTitleP1 = new JLabel();
 	private JLabel lblTitleP2 = new JLabel();
-	private JLabel lblUsername = new JLabel();
 	private JLabel lblUsernameP1 = new JLabel();
 	private JLabel lblUsernameP2 = new JLabel();
 	private JLabel lblTitle = new JLabel();
+	private JLabel lbltfBackground = new JLabel();
+	private JLabel lblEnter = new JLabel();
 	private JTextField tf = new JTextField();
 	private JTextField tfP1 = new JTextField("Enter Player 1's name");
 	private JTextField tfP2 = new JTextField("Enter Player 2's name");
 	private Font r60 = new Font("Rockwell", Font.PLAIN, 60);
 	private Font p50 = new Font("Papyrus", Font.BOLD, 50);
 	private Font r20 = new Font("Rockwell", Font.PLAIN, 20);
+	private Font bof18 = new Font("Baskerville Old Face", Font.BOLD, 18);
+
 	private Border blackline, raisedetched, loweredetched, raisedbevel,
 			loweredbevel, empty;
 	private boolean singlePlayer;
@@ -70,20 +69,37 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 		contentPanel.setLayout(null);
 		contentPanel.setPreferredSize(new Dimension(1000, 700));
 
-		btnEasy.setBounds(311, 420, 100, 40);
-		btnMedium.setBounds(441, 420, 100, 40);
-		btnHard.setBounds(571, 420, 100, 40);
-		btnStart.setBounds(311, 540, 360, 110);
-		btnStart.setFont(p50);
+		// imageicons
+		ImageIcon iconbtnEasy = new ImageIcon("Images/easy2.png");
+		btnEasy = new JButton(iconbtnEasy);
+		ImageIcon iconbtnMedium = new ImageIcon("Images/medium.png");
+		btnMedium = new JButton(iconbtnMedium);
+		ImageIcon iconbtnHard = new ImageIcon("Images/hard.png");
+		btnHard = new JButton(iconbtnHard);
+		ImageIcon iconbtnStart = new ImageIcon("Images/Start.jpg");
+		btnStart = new JButton(iconbtnStart);
+		ImageIcon iconbtnStandard = new ImageIcon("Images/Standard.jpg");
+		btnStandard = new JButton(iconbtnStandard);
+		ImageIcon iconbtnFlag = new ImageIcon("Images/Flag.jpg");
+		btnFlag = new JButton(iconbtnFlag);
+		ImageIcon iconbtnPlus = new ImageIcon("Images/Plus.jpg");
+		btnPlus = new JButton(iconbtnPlus);
 		ImageIcon iconSettings = new ImageIcon("Images/cog2.png");
 		btnSettings = new JButton(iconSettings);
+		
+	
+		btnStandard.setBounds(313, 400, 98, 76);
+		btnFlag.setBounds(442, 400, 98, 76);
+		btnPlus.setBounds(571, 400, 98, 76);
+		btnEasy.setBounds(311, 520, 100, 40);
+		btnMedium.setBounds(441, 520, 100, 40);
+		btnHard.setBounds(571, 520, 100, 40);
+		btnStart.setBounds(336, 590, 312, 59);
 		btnSettings.setBounds(10, 10, 50, 50);
 
 		// mainPanel
 		mainPanel.setBounds(311, 30, 460, 400);
 		mainPanel.setOpaque(false);
-		lblUsername.setBounds(50, 150, 1500, 95);
-		lblUsername.setFont(r20);
 		lblTitle.setBounds(0, 0, 1000, 90);
 		lblTitle.setFont(r60);
 
@@ -95,6 +111,9 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 		btnSettings.setContentAreaFilled(false);
 
 		// add components
+		contentPanel.add(btnStandard);
+		contentPanel.add(btnPlus);
+		contentPanel.add(btnFlag);
 		contentPanel.add(btnEasy);
 		contentPanel.add(btnMedium);
 		contentPanel.add(btnHard);
@@ -105,18 +124,32 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 		// singleplayer
 
 		if (singlePlayer == true) {
-			lblTitle.setText("Singleplayer");
-			mainPanel.add(lblTitle);
-			lblUsername.setText("Enter your name below");
+
+			
+			
+			ImageIcon iconlblTitle = new ImageIcon("Images/Title.png");
+			lblTitle = new JLabel(iconlblTitle);
+			lblTitle.setBounds(0, 0, 380, 90);
 			mainPanel.add(tf);
-			mainPanel.add(lblUsername);
+			mainPanel.add(lblTitle);
+			ImageIcon iconlblEnter = new ImageIcon("Images/Enter.png");
+			lblEnter = new JLabel(iconlblEnter);
+			lblEnter.setBounds(50, 195, 220, 40);
+			mainPanel.add(lblEnter);
+		
+			ImageIcon icontfBackground = new ImageIcon("Images/TextFieldBackground3.jpg");
+			lbltfBackground = new JLabel(icontfBackground);
+			lbltfBackground.setBounds(80, 230, 150, 50);
+			mainPanel.add(lbltfBackground);
+
 			// set stuff invisible
-			tf.setBounds(100, 200, 150, 60);
+			tf.setBounds(85, 230, 140, 50);
+			tf.setFont(bof18);
 			tf.setBorder(null);
 			tf.setMargin(new Insets(0, 0, 0, 0));
 			tf.setOpaque(false);
 		}
-
+		
 		// multiplayer
 		else {
 			ImageIcon iconMP = new ImageIcon("Images/multiplayer.png");
@@ -132,6 +165,8 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 			lblUsernameP2.setBounds(225, 150, 150, 60);
 			mainPanel.add(tfP1);
 			mainPanel.add(tfP2);
+			mainPanel.add(lblUsernameP1);
+			mainPanel.add(lblUsernameP2);
 		}
 		
 		// set textfield invisible and bounds
@@ -145,8 +180,6 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 		tfP2.setMargin(new Insets(0, 0, 0, 0));
 		tfP2.setOpaque(false);
 
-		mainPanel.add(lblUsernameP1);
-		mainPanel.add(lblUsernameP2);
 
 		// actionListener
 		btnSettings.addActionListener(this);
@@ -160,7 +193,7 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 
 	private class BgPanel extends JPanel {
 		private java.awt.Image bg = new ImageIcon(
-				"Images/chooserBackground.jpg").getImage();
+				"Images/retro3.jpg").getImage();
 
 		@Override
 		public void paintComponent(Graphics g) {
