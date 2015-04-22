cbfb459287674906b340c5109616fa92a9883e47
Joakim1331 <joakim_naw@hotmail.com>
2015-04-22 10:49:30 +0200

    fixat multiplayer och singleplayer, gjort nya bilder tagit bort gamla.. edittat position på ALLT

--- src/gui/ChooseGameGUI.java
+++ src/gui/ChooseGameGUI.java
@@ -41,7 +41,10 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 	private JLabel lblUsernameP2 = new JLabel();
 	private JLabel lblTitle = new JLabel();
 	private JLabel lbltfBackground = new JLabel();
+	private JLabel lbltfBackground2 = new JLabel();
 	private JLabel lblEnter = new JLabel();
+	private JLabel lblTheme = new JLabel();
+	private JLabel lblDifficulty = new JLabel();
 	private JTextField tf = new JTextField();
 	private JTextField tfP1 = new JTextField("Enter Player 1's name");
 	private JTextField tfP2 = new JTextField("Enter Player 2's name");
@@ -70,6 +73,18 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 		contentPanel.setPreferredSize(new Dimension(1000, 700));
 
 		// imageicons
+		ImageIcon iconSettings = new ImageIcon("Images/cog2.png");
+		btnSettings = new JButton(iconSettings);
+		ImageIcon iconlblTheme = new ImageIcon("Images/Theme.png");
+		lblTheme = new JLabel(iconlblTheme);
+		ImageIcon iconbtnStandard = new ImageIcon("Images/Standard.jpg");
+		btnStandard = new JButton(iconbtnStandard);
+		ImageIcon iconbtnFlag = new ImageIcon("Images/Flag.jpg");
+		btnFlag = new JButton(iconbtnFlag);
+		ImageIcon iconbtnPlus = new ImageIcon("Images/Plus.jpg");
+		btnPlus = new JButton(iconbtnPlus);
+		ImageIcon iconlblDifficulty = new ImageIcon("Images/Difficulty.png");
+		lblDifficulty = new JLabel(iconlblDifficulty);
 		ImageIcon iconbtnEasy = new ImageIcon("Images/easy2.png");
 		btnEasy = new JButton(iconbtnEasy);
 		ImageIcon iconbtnMedium = new ImageIcon("Images/medium.png");
@@ -78,24 +93,17 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 		btnHard = new JButton(iconbtnHard);
 		ImageIcon iconbtnStart = new ImageIcon("Images/Start.jpg");
 		btnStart = new JButton(iconbtnStart);
-		ImageIcon iconbtnStandard = new ImageIcon("Images/Standard.jpg");
-		btnStandard = new JButton(iconbtnStandard);
-		ImageIcon iconbtnFlag = new ImageIcon("Images/Flag.jpg");
-		btnFlag = new JButton(iconbtnFlag);
-		ImageIcon iconbtnPlus = new ImageIcon("Images/Plus.jpg");
-		btnPlus = new JButton(iconbtnPlus);
-		ImageIcon iconSettings = new ImageIcon("Images/cog2.png");
-		btnSettings = new JButton(iconSettings);
-		
-	
-		btnStandard.setBounds(313, 400, 98, 76);
-		btnFlag.setBounds(442, 400, 98, 76);
-		btnPlus.setBounds(571, 400, 98, 76);
-		btnEasy.setBounds(311, 520, 100, 40);
-		btnMedium.setBounds(441, 520, 100, 40);
-		btnHard.setBounds(571, 520, 100, 40);
-		btnStart.setBounds(336, 590, 312, 59);
+
 		btnSettings.setBounds(10, 10, 50, 50);
+		lblTheme.setBounds(340, 210, 288, 46);
+		btnStandard.setBounds(313, 275, 98, 76);
+		btnFlag.setBounds(442, 275, 98, 76);
+		btnPlus.setBounds(571, 275, 98, 76);
+		lblDifficulty.setBounds(350, 360, 304, 39);
+		btnEasy.setBounds(311, 420, 100, 40);
+		btnMedium.setBounds(441, 420, 100, 40);
+		btnHard.setBounds(571, 420, 100, 40);
+		btnStart.setBounds(336, 500, 312, 59);
 
 		// mainPanel
 		mainPanel.setBounds(311, 30, 460, 400);
@@ -111,13 +119,15 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 		btnSettings.setContentAreaFilled(false);
 
 		// add components
+		contentPanel.add(btnSettings);
+		contentPanel.add(lblTheme);
 		contentPanel.add(btnStandard);
-		contentPanel.add(btnPlus);
 		contentPanel.add(btnFlag);
+		contentPanel.add(btnPlus);
+		contentPanel.add(lblDifficulty);
 		contentPanel.add(btnEasy);
 		contentPanel.add(btnMedium);
 		contentPanel.add(btnHard);
-		contentPanel.add(btnSettings);
 		contentPanel.add(btnStart);
 		contentPanel.add(mainPanel);
 
@@ -134,16 +144,16 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 			mainPanel.add(lblTitle);
 			ImageIcon iconlblEnter = new ImageIcon("Images/Enter.png");
 			lblEnter = new JLabel(iconlblEnter);
-			lblEnter.setBounds(50, 195, 220, 40);
+			lblEnter.setBounds(70, 90, 228, 64);
 			mainPanel.add(lblEnter);
 		
 			ImageIcon icontfBackground = new ImageIcon("Images/TextFieldBackground3.jpg");
 			lbltfBackground = new JLabel(icontfBackground);
-			lbltfBackground.setBounds(80, 230, 150, 50);
+			lbltfBackground.setBounds(105, 127, 150, 50);
 			mainPanel.add(lbltfBackground);
 
 			// set stuff invisible
-			tf.setBounds(85, 230, 140, 50);
+			tf.setBounds(110, 130, 140, 50);
 			tf.setFont(bof18);
 			tf.setBorder(null);
 			tf.setMargin(new Insets(0, 0, 0, 0));
@@ -156,26 +166,35 @@ public class ChooseGameGUI extends JPanel implements ActionListener {
 
 			lblTitle.setIcon(iconMP);
 			mainPanel.add(lblTitle);
-			ImageIcon iconP1 = new ImageIcon("Images/txt_player1_150x50.png");
-			ImageIcon iconP2 = new ImageIcon("Images/txt_player2_150x50.png");
+			ImageIcon iconP1 = new ImageIcon("Images/Player1.png");
+			ImageIcon iconP2 = new ImageIcon("Images/Player2.png");
 			lblUsernameP1.setIcon(iconP1);
 			lblUsernameP2.setIcon(iconP2);
 
-			lblUsernameP1.setBounds(0, 150, 150, 60);
-			lblUsernameP2.setBounds(225, 150, 150, 60);
+			lblUsernameP1.setBounds(0, 70, 150, 60);
+			lblUsernameP2.setBounds(225, 70, 150, 60);
 			mainPanel.add(tfP1);
 			mainPanel.add(tfP2);
 			mainPanel.add(lblUsernameP1);
 			mainPanel.add(lblUsernameP2);
+			
+			ImageIcon icontfBackground = new ImageIcon("Images/TextFieldBackground3.jpg");
+			lbltfBackground = new JLabel(icontfBackground);
+			lbltfBackground.setBounds(0, 105, 150, 65);
+			lbltfBackground2 = new JLabel(icontfBackground);
+			lbltfBackground2.setBounds(220, 105, 150, 65);
+			mainPanel.add(lbltfBackground);
+			mainPanel.add(lbltfBackground2);
+			
 		}
 		
 		// set textfield invisible and bounds
-		tfP1.setBounds(0, 200, 150, 60);
+		tfP1.setBounds(5, 110, 150, 60);
 		tfP1.setBorder(null);
 		tfP1.setMargin(new Insets(0, 0, 0, 0));
 		tfP1.setOpaque(false);
 
-		tfP2.setBounds(225, 200, 150, 60);
+		tfP2.setBounds(225, 110, 150, 60);
 		tfP2.setBorder(null);
 		tfP2.setMargin(new Insets(0, 0, 0, 0));
 		tfP2.setOpaque(false);
